4e22fe2b25c335928f9c3bc628512d9e4441dba9
Ybz1234 <jonathanbz49@gmail.com>
2024-12-14 20:14:02 +0200

    End of section 8

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,13 +1,14 @@
 // import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'semantic-ui-css/semantic.min.css';
-import App from './app/layout/App.tsx'
 import './app/layout/styles.css';
 import { store, StoreContext } from './app/stores/store.ts';
+import { RouterProvider } from 'react-router-dom';
+import { Router } from './app/router/Routes.tsx';
 
 createRoot(document.getElementById('root')!).render(
   // <StrictMode> just add a closing tag
     <StoreContext.Provider value={store}>
-      <App />
+      <RouterProvider router={Router} />
     </StoreContext.Provider>
 )
