1aba6497f3f38742f787207d5b9f4d7b94f4d715
Ybz1234 <jonathanbz49@gmail.com>
2024-12-22 15:04:51 +0200

    End of seection 14

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -12,5 +12,14 @@ namespace Persistence
         }
 
         public DbSet<Activity> Activities {  get; set; }
+        public DbSet<ActivityAttendee> ActivityAttendees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<ActivityAttendee>(x => x.HasKey(aa => new { aa.AppUserId, aa.ActivityId }));
+            modelBuilder.Entity<ActivityAttendee>().HasOne(u => u.AppUser).WithMany(a => a.Activities).HasForeignKey(aa => aa.AppUserId);
+            modelBuilder.Entity<ActivityAttendee>().HasOne(u => u.Activity).WithMany(a => a.Attendees).HasForeignKey(aa => aa.ActivityId);
+        }
     }
 }
